f56ec85afe62fc2337cc0a262b5f1866fce1e6ea
roechi <msg.remus@gmail.com>
2016-11-01 13:24:05 +0100

    PIMP-1 - adds basic rest application with example controller

---
+++ src/main/java/com/pimp/PimpRestApplication.java
@@ -0,0 +1,12 @@
+package com.pimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PimpRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PimpRestApplication.class, args);
+	}
+}
---
+++ src/main/java/com/pimp/domain/Message.java
@@ -0,0 +1,14 @@
+package com.pimp.domain;
+
+public class Message {
+
+    private String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
