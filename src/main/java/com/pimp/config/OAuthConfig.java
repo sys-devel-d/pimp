4036f5fa14b7c87152afd4ba0fbb9bd9596af89e
roechi <msg.remus@gmail.com>
2016-11-12 20:50:46 +0100

    PIMP-9 - configures auth and resource server

--- src/main/java/com/pimp/config/OAuthConfig.java
+++ src/main/java/com/pimp/config/OAuthConfig.java
@@ -3,37 +3,82 @@ package com.pimp.config;
 import com.pimp.services.CustomClientDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
-@EnableAuthorizationServer
-public class OAuthConfig extends AuthorizationServerConfigurerAdapter {
+public class OAuthConfig {
 
-    private final String URL_PREFIX = "/api";
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceSerConfig extends ResourceServerConfigurerAdapter {
 
-    @Autowired
-    @Qualifier(value = "authenticationManagerBean")
-    private AuthenticationManager authenticationManager;
+        private final String RESOURCE_ID = "PIMP_RESOURCE";
 
-    @Autowired
-    private CustomClientDetailsService clientDetailsService;
+        @Autowired
+        private TokenStore tokenStore;
 
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.withClientDetails(clientDetailsService);
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+            resources.tokenStore(tokenStore)
+            .resourceId(RESOURCE_ID);
+        }
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().authenticated();
+        }
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+        private final String URL_PREFIX = "/api";
+
+        @Autowired
+        private TokenStore tokenStore;
+
+        @Autowired
+        @Qualifier(value = "authenticationManagerBean")
+        private AuthenticationManager authenticationManager;
+
+        @Autowired
+        private CustomClientDetailsService clientDetailsService;
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.withClientDetails(clientDetailsService);
+        }
+
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints
+                    .pathMapping("/oauth/token", URL_PREFIX + "/oauth/token")
+                    .authenticationManager(authenticationManager)
+                    .tokenStore(tokenStore);
+        }
     }
 
 
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints
-                .pathMapping("/oauth/token", URL_PREFIX + "/oauth/token")
-                .authenticationManager(authenticationManager);
-    }
+    @Configuration
+    public class TokenStoreConfig {
 
+        @Bean
+        public TokenStore tokenStore() {
+            return new InMemoryTokenStore();
+        }
+    }
 }
--- src/main/java/com/pimp/config/SecurityConfig.java
+++ src/main/java/com/pimp/config/SecurityConfig.java
@@ -5,45 +5,66 @@ import com.pimp.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 
-@EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
+public class SecurityConfig {
 
-    @Autowired
-    private PasswordEncoder encoder;
+    @Configuration
+    public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private CustomUserDetailsService userDetailsService;
+        @Autowired
+        private PasswordEncoder encoder;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        @Autowired
+        private CustomUserDetailsService userDetailsService;
 
-        http
-                .authorizeRequests().anyRequest().permitAll()
-            .and()
-                .addFilterBefore(new SimpleCorsFilter(), SecurityContextPersistenceFilter.class).antMatcher("/api/*");
+        @Autowired
+        public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+        }
+
+        @Override
+        @Bean
+        public AuthenticationManager authenticationManagerBean()
+                throws Exception {
+            return super.authenticationManagerBean();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            http
+                    .authorizeRequests().anyRequest().permitAll()
+                    .and()
+                    .addFilterBefore(new SimpleCorsFilter(), SecurityContextPersistenceFilter.class).antMatcher("/api/*");
+        }
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
-    }
+    @Configuration
+    public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
 
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean()
-            throws Exception {
-        return super.authenticationManagerBean();
+        @Autowired
+        private WebSecurityConfiguration webSecurityConfiguration;
+
+        @Override
+        protected MethodSecurityExpressionHandler createExpressionHandler() {
+            return new OAuth2MethodSecurityExpressionHandler();
+        }
     }
 
     @Configuration
