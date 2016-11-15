a26580011ebdf266fc807e6f6ebaabfefef5a031
roechi <msg.remus@gmail.com>
2016-11-15 12:57:23 +0100

    DEBT - enhances access token with user name

--- src/main/java/com/pimp/config/OAuthConfig.java
+++ src/main/java/com/pimp/config/OAuthConfig.java
@@ -5,8 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.token.AccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordAccessTokenProvider;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -14,8 +20,17 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.password.ResourceOwnerPasswordTokenGranter;
+import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class OAuthConfig {
@@ -32,7 +47,7 @@ public class OAuthConfig {
         @Override
         public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
             resources.tokenStore(tokenStore)
-            .resourceId(RESOURCE_ID);
+                    .resourceId(RESOURCE_ID);
         }
 
         @Override
@@ -55,18 +70,34 @@ public class OAuthConfig {
         @Autowired
         private CustomClientDetailsService clientDetailsService;
 
+        @Autowired
+        private AuthorizationServerTokenServices authorizationServerTokenServices;
+
+        @Autowired
+        private TokenEnhancer tokenEnhancer;
+
         @Override
         public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
             clients.withClientDetails(clientDetailsService);
         }
 
-
         @Override
         public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            DefaultOAuth2RequestFactory requestFactory = new DefaultOAuth2RequestFactory(clientDetailsService);
+
             endpoints
                     .pathMapping("/oauth/token", "/oauth/token")
+                    .tokenStore(tokenStore)
                     .authenticationManager(authenticationManager)
-                    .tokenStore(tokenStore);
+                    .tokenServices(authorizationServerTokenServices)
+                    .requestFactory(requestFactory)
+                    .tokenEnhancer(tokenEnhancer)
+                    .tokenGranter(new ResourceOwnerPasswordTokenGranter(
+                            authenticationManager,
+                            authorizationServerTokenServices,
+                            clientDetailsService,
+                            requestFactory
+                    ));
         }
     }
 
@@ -77,6 +108,42 @@ public class OAuthConfig {
         @Bean
         public TokenStore tokenStore() {
             return new InMemoryTokenStore();
+        }
+
+        @Bean
+        AccessTokenProvider accessTokenProvider() {
+            ResourceOwnerPasswordAccessTokenProvider tokenProvider = new ResourceOwnerPasswordAccessTokenProvider();
+            return tokenProvider;
+        }
+
+        @Bean
+        @Primary
+        @Autowired
+        AuthorizationServerTokenServices authorizationServerTokenServices(TokenStore tokenStore, TokenEnhancer tokenEnhancer) {
+            DefaultTokenServices tokenServices = new DefaultTokenServices();
+            tokenServices.setTokenStore(tokenStore);
+            tokenServices.setSupportRefreshToken(true);
+            tokenServices.setTokenEnhancer(tokenEnhancer);
+            return tokenServices;
+        }
+
+        @Bean
+        TokenEnhancer tokenEnhancer() {
+            return new TokenEnhancer() {
+                @Override
+                public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+                    if (!(accessToken instanceof DefaultOAuth2AccessToken)) {
+                        throw new IllegalArgumentException("Token must be a DefaultOAuth2AccessToken");
+                    }
+                    DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) accessToken;
+                    UserDetails user = (UserDetails) authentication.getPrincipal();
+                    Map<String, Object> additionalInformation = new HashMap<>();
+                    additionalInformation.put("user_name", user.getUsername());
+                    token.setAdditionalInformation(additionalInformation);
+
+                    return token;
+                }
+            };
         }
     }
 }
