b2a0089c3ba884a34c8228d7d5b5c57b3a7fc389
roechi <msg.remus@gmail.com>
2016-11-16 00:25:20 +0100

    DEBT - adds tests

---
+++ src/test/java/com/pimp/services/CustomTokenEnhancerServiceTest.java
@@ -0,0 +1,28 @@
+package com.pimp.services;
+
+import com.pimp.domain.User;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+public class CustomTokenEnhancerServiceTest {
+
+    private CustomTokenEnhancerService tokenEnhancer = new CustomTokenEnhancerService();
+
+    @Test
+    public void testEnhancesTokenWithUserName() throws Exception {
+        DefaultOAuth2AccessToken token = new DefaultOAuth2AccessToken(DefaultOAuth2AccessToken.ACCESS_TOKEN);
+        OAuth2Authentication authentication = Mockito.mock(OAuth2Authentication.class);
+        when(authentication.getPrincipal()).thenReturn(new User().setUserName("fooBar"));
+
+        OAuth2AccessToken enhanced = tokenEnhancer.enhance(token, authentication);
+
+        assertThat(enhanced.getAdditionalInformation().containsKey("user_name"));
+        assertThat(enhanced.getAdditionalInformation().get("user_name")).isEqualTo("fooBar");
+    }
+}
