7fcf8ac461a7899becde28502a1e95b5bd795792
Jakob Pupke <jakob.pupke@gmail.com>
2016-11-30 01:31:51 +0100

    remove subscribe mapping in chat. send users rooms on first request instead of just strings.

--- src/main/java/com/pimp/controller/RoomController.java
+++ src/main/java/com/pimp/controller/RoomController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -28,12 +25,10 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @RequestMapping("/rooms")
 public class RoomController {
 
-    private UserService userService;
     private ChatRoomService chatRoomService;
 
     @Autowired
-    public RoomController(UserService userService, ChatRoomService chatRoomService) {
-        this.userService = userService;
+    public RoomController(ChatRoomService chatRoomService) {
         this.chatRoomService = chatRoomService;
     }
 
@@ -56,12 +51,10 @@ public class RoomController {
     }
 
     @PreAuthorize("#oauth2.hasScope('user_actions')")
-    @RequestMapping(method = GET, path = "/{userName}/rooms")
-    public List<String> getUsersRooms(@PathVariable String userName) {
-        User user = userService.findByUserName(userName);
-        // Maybe return a list of ChatRooms instead of sending only the room names
-        List<String> rooms = chatRoomService.findUsersRooms(user)
-                .stream().map(ChatRoom::getRoomName).collect(Collectors.toList());
+    @RequestMapping(method = GET, path = "/")
+    public List<ChatRoom> getUsersRooms() {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        List<ChatRoom> rooms = chatRoomService.findUsersRooms(user);
         if(rooms == null) return new ArrayList<>();
         return rooms;
     }
