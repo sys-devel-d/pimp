8a8d41dc2a5c0b758723f715cb45c590e0c239da
roechi <msg.remus@gmail.com>
2016-12-09 18:33:37 +0100

    PIMP-75 - adds controller methods and tests to add/remove users

--- src/main/java/com/pimp/controller/ProjectController.java
+++ src/main/java/com/pimp/controller/ProjectController.java
@@ -1,7 +1,9 @@
 package com.pimp.controller;
 
+import com.pimp.commons.exceptions.EntityNotFoundException;
 import com.pimp.domain.Project;
 import com.pimp.services.ProjectService;
+import com.pimp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,10 +19,12 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 public class ProjectController {
 
     private final ProjectService projectService;
+    private final UserService userService;
 
     @Autowired
-    public ProjectController(ProjectService projectService) {
+    public ProjectController(ProjectService projectService, UserService userService) {
         this.projectService = projectService;
+        this.userService = userService;
     }
 
     @RequestMapping(method = GET, path = "/{name}")
@@ -36,5 +40,23 @@ public class ProjectController {
     @RequestMapping(method = DELETE, path = "/{name}")
     public void delete(@PathVariable String name) {
         projectService.delete(name);
+    }
+
+    @RequestMapping(method = PATCH, path = "/{projectName}/{userName}")
+    public void add(@PathVariable String projectName, @PathVariable String userName) {
+        if (!userService.exists(userName)) {
+            throw new EntityNotFoundException("User " + userName + "does not exist.");
+        }
+
+        projectService.add(projectName, userName);
+    }
+
+    @RequestMapping(method = DELETE, path = "/{projectName}/{userName}")
+    public void remove(@PathVariable String projectName, @PathVariable String userName) {
+        if (!userService.exists(userName)) {
+            throw new EntityNotFoundException("User " + userName + "does not exist.");
+        }
+
+        projectService.remove(projectName, userName);
     }
 }
