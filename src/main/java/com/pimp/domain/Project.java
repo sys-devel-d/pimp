1e797d15e26bf0dbab2969f7a5fea1ec8ba3f66f
roechi <msg.remus@gmail.com>
2016-12-09 18:31:33 +0100

    PIMP-75 - refactors project to extend IKeyedObject

--- src/main/java/com/pimp/domain/Project.java
+++ src/main/java/com/pimp/domain/Project.java
@@ -2,25 +2,27 @@ package com.pimp.domain;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.pimp.commons.mongo.IKeyedObject;
+import org.bson.types.ObjectId;
 import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 
 import java.util.LinkedList;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_ABSENT)
-public class Project {
+public class Project implements IKeyedObject{
 
     @JsonProperty
+    @Id
+    private String key = new ObjectId().toString();
+    @JsonProperty
     @NotEmpty
+    @Indexed(unique = true)
     private String name;
     @JsonProperty
     private List<String> userNames = new LinkedList<>();
-
-    public static Project from(ProjectDocument document) {
-        return new Project()
-                .setName(document.getName())
-                .setUserNames(document.getUserNames());
-    }
 
     public String getName() {
         return name;
@@ -41,6 +43,16 @@ public class Project {
     }
 
     @Override
+    public String getKey() {
+        return key;
+    }
+
+    @Override
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -57,5 +69,17 @@ public class Project {
         int result = name != null ? name.hashCode() : 0;
         result = 31 * result + (userNames != null ? userNames.hashCode() : 0);
         return result;
+    }
+
+    public Project add(String userName) {
+        userNames.add(userName);
+
+        return this;
+    }
+
+    public Project remove(String userName) {
+        userNames.remove(userName);
+
+        return this;
     }
 }
