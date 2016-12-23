95400655f3015bfbdb3d94bedfbd3d3bb96d665d
roechi <msg.remus@gmail.com>
2016-12-23 21:36:09 +0100

    PIMP-77 - enables token enhancer to add admin scope for admin users

--- src/test/java/com/pimp/services/CustomTokenEnhancerServiceTest.java
+++ src/test/java/com/pimp/services/CustomTokenEnhancerServiceTest.java
@@ -7,6 +7,8 @@ import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -24,5 +26,20 @@ public class CustomTokenEnhancerServiceTest {
 
         assertThat(enhanced.getAdditionalInformation().containsKey("user_name"));
         assertThat(enhanced.getAdditionalInformation().get("user_name")).isEqualTo("fooBar");
+    }
+
+    @Test
+    public void testEnhanceWithAdminScope() throws Exception {
+        DefaultOAuth2AccessToken token = new DefaultOAuth2AccessToken(DefaultOAuth2AccessToken.ACCESS_TOKEN);
+        OAuth2Authentication authentication = Mockito.mock(OAuth2Authentication.class);
+        when(authentication.getPrincipal()).thenReturn(
+                new User()
+                        .setUserName("fooBar")
+                        .setRoles(Arrays.asList("ADMIN")));
+
+        OAuth2AccessToken enhanced = tokenEnhancer.enhance(token, authentication);
+
+        assertThat(enhanced.getScope())
+                .containsAll((Arrays.asList("user_actions", "admin_actions")));
     }
 }
