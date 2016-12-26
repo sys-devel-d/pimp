f0b43d45c9d5db42dee2f38ffc8be2613ed9d078
roechi <msg.remus@gmail.com>
2016-12-26 14:14:50 +0100

    PIMP-77 - sets permissions for admin endpoints

--- src/main/java/com/pimp/controller/TeamController.java
+++ src/main/java/com/pimp/controller/TeamController.java
@@ -5,8 +5,12 @@ import com.pimp.services.TeamService;
 import com.pimp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(path = "/team")
@@ -16,5 +20,29 @@ public class TeamController extends GroupController<Team> {
     @Autowired
     public TeamController(TeamService teamService, UserService userService) {
         super(teamService, userService);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @Override
+    public void create(@Valid @RequestBody Team group) {
+        super.create(group);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @Override
+    public void delete(@PathVariable String name) {
+        super.delete(name);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @Override
+    public void add(@PathVariable String groupName, @PathVariable String userName) {
+        super.add(groupName, userName);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @Override
+    public void remove(@PathVariable String groupName, @PathVariable String userName) {
+        super.remove(groupName, userName);
     }
 }
