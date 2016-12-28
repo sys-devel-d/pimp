110f39264b83ffd369831464e2eed3e2b9adb647
roechi <msg.remus@gmail.com>
2016-12-28 20:43:21 +0100

    PIMP-86 -  adds web socket broker for notifications

--- src/main/java/com/pimp/services/NotificationDispatcherService.java
+++ src/main/java/com/pimp/services/NotificationDispatcherService.java
@@ -1,14 +1,18 @@
 package com.pimp.services;
 
+import com.pimp.commons.exceptions.EntityAlreadyExistsException;
+import com.pimp.commons.exceptions.EntityNotFoundException;
 import com.pimp.domain.Notification;
 import com.pimp.domain.NotificationChannel;
 import com.pimp.domain.NotificationChannelDocument;
 import com.pimp.repositories.NotificationChannelRepo;
 import org.bson.types.ObjectId;
+import org.springframework.stereotype.Service;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.time.Instant;
 
+@Service
 public class NotificationDispatcherService {
 
     private NotificationChannelRepo repo;
@@ -27,13 +31,15 @@ public class NotificationDispatcherService {
     }
 
     private Notification addNotification(Notification notification) {
-        if (!repo.exists(notification.getRoomId())) {
-            NotificationChannelDocument document = repo.findByRoomName(notification.getRoomId());
+        if (repo.exists(notification.getRoomId())) {
+            NotificationChannelDocument document = (NotificationChannelDocument) repo.findOne(notification.getRoomId());
             notification.setCreationDate(Instant.now());
             notification.setKey(new ObjectId().toString());
             NotificationChannel channel = (NotificationChannel) NotificationChannel.from(document);
             channel.addMessage(notification);
             repo.save(NotificationChannelDocument.from(channel));
+        } else {
+          throw new EntityNotFoundException("Could not find channel with id " + notification.getRoomId() + ".");
         }
         return notification;
     }
@@ -60,4 +66,22 @@ public class NotificationDispatcherService {
                 .map(message -> ((Notification) message).setAcknowledged(notification.isAcknowledged()));
         repo.save(channel);
     }
+
+    public NotificationChannel find(String channelName) {
+      if (repo.exists(channelName)) {
+        NotificationChannelDocument document = repo.findByRoomName(channelName);
+        return NotificationChannel.from(document);
+      } else {
+        throw new EntityNotFoundException("A channel with the name " + channelName + " does not exist.");
+      }
+    }
+
+    public NotificationChannelDocument create(NotificationChannel channel) {
+      if (repo.exists(channel.getRoomName())) {
+        throw new EntityAlreadyExistsException("A channel with the name " + channel.getRoomName() + " already exists.");
+      }
+
+      return repo.save(NotificationChannelDocument.from(channel));
+    }
+
 }
