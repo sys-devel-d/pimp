50af09eda151caacfddd9b0febf37263060f8c63
roechi <msg.remus@gmail.com>
2016-12-29 16:51:05 +0100

    PIMP-86 -  alters message type hierarchy, adds simple mapping test

--- src/main/java/com/pimp/domain/NotificationMessage.java
+++ src/main/java/com/pimp/domain/NotificationMessage.java
@@ -1,5 +1,46 @@
 package com.pimp.domain;
 
 
-public interface NotificationMessage {
+public class NotificationMessage {
+
+  private String sendingUser;
+  private String receivingUser;
+  private String content;
+  private String chatKey;
+
+  public String getSendingUser() {
+    return sendingUser;
+  }
+
+  public NotificationMessage setSendingUser(String sendingUser) {
+    this.sendingUser = sendingUser;
+    return this;
+  }
+
+  public String getReceivingUser() {
+    return receivingUser;
+  }
+
+  public NotificationMessage setReceivingUser(String receivingUser) {
+    this.receivingUser = receivingUser;
+    return this;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public NotificationMessage setContent(String content) {
+    this.content = content;
+    return this;
+  }
+
+  public String getChatKey() {
+    return chatKey;
+  }
+
+  public NotificationMessage setChatKey(String chatKey) {
+    this.chatKey = chatKey;
+    return this;
+  }
 }
